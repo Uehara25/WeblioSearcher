8978208bdff59a9ab797c4e31922febdb7cf5425
Uehara25 <akiya.noface@gmail.com>
2015-10-09 23:55:11 +0900

    テストプログラム
    仕様が確定したら書く。

---
+++ test_WeblioSearcher.py
@@ -0,0 +1,19 @@
+# Script Name	: test_WeblioSearcher.py
+# Author		: Uehara25
+# Created		: 
+# Last Modified : 
+# Version		: 
+
+
+# Modifications	: 
+
+# Description	: WeblioSearcher.py のテストプログラム
+
+
+# -*- coding: utf-8 -*-
+
+""" 仕様が確定してないため、まだ書きようがない """
+
+import unittest
+
+# ここまで
