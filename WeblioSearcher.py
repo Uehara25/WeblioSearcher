418b64e46048d69287e1f881f57ed1fec6e70623
Uehara25 <akiya.noface@gmail.com>
2015-10-10 23:06:11 +0900

    WeblioSearcherクラスを消して、getmeaning(word)モジュールのみにした。

--- WeblioSearcher.py
+++ WeblioSearcher.py
@@ -8,32 +8,26 @@
 
 # Modifications	: 
 
-# Description	: 英単語の意味ををWeblioから取得する。
+# Description	: 英単語の意味ををWeblio英和・和英辞典から取得する。
 
 
 
-import codecs
 import sys
-import requests
 import io
+import requests
 
 import meaning
 import weblio_parser
 
-class WeblioSearcher:
-	def __init__(self):
-		""" 初期化 """
-		self.weblioURL = "http://ejje.weblio.jp/content/"
 
-	def getmeaning(self, word):
-		""" Meaningオブジェクトを返す。　"""
+def getmeaning(word):
+	""" 英単語を引数で受け取り、Meaningオブジェクトを返す """
 
-		self.parser = weblio_parser.WeblioParser()
-		self.data = None
-		src = requests.get(self.weblioURL + word)
-		self.parser.feed(src.text)
-		self.data = self.parser.getdata()
-		return meaning.Meaning(self.data)
+	parser = weblio_parser.WeblioParser()
+	src = requests.get("http://ejje.weblio.jp/content/" + word)
+	parser.feed(src.text)
+	data = parser.getdata()
+	return meaning.Meaning(data)
 
 
 if __name__ == "__main__":
@@ -48,8 +42,7 @@ if __name__ == "__main__":
 		print(u"何も聞かれてないから、何も答えないよ")
 	else:
 		sys.argv.pop(0)
-		searcher = WeblioSearcher()
 		for word in sys.argv:
-			it_mean = searcher.getmeaning(word)
+			it_mean = getmeaning(word)
 			print("[", word, "]")
 			print(it_mean.main, end='\n\n')
